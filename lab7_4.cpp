02894f78e5261e191280a836f0c9370bdac7c0ae
Natan <natan_bunkerd@cmu.ac.th>
2022-12-13 16:41:50 +0700

    lab 7-4

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -9,7 +9,20 @@ int main(){
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
-    //Write your code here.
+    cout << "Please input GPA: ";
+	double key;
+	cin >> key;
+	int i = 0;
+	int N = 10;
+    while(i < N){
+        if(gpa[i] >= key){
+            cout << id[i] <<" ";
+			cout << name[i] <<" ";
+			cout << gpa[i] ;
+			cout << "\n";
+        }
+        i++;
+	}
 		
 	return 0;
 }
