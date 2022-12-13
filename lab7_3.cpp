ee54a08e6e7fe84c6854a176981f209083ccf60e
Natan <natan_bunkerd@cmu.ac.th>
2022-12-13 16:26:02 +0700

    lab 7-3

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
 
 using namespace std;
-
+int a(int x, int y){
+  int c;
+  if(x > y){
+    c = x- y;
+  }else{
+    c = y-x;
+  }
+  return c;
+}
+int adiff(int x,int y){
+  int x1 = x%360,y1 = y%360,c1,c2,r;
+  c1 = a(x1,360);
+  c2 = a(y1,360);
+  r = a(c1,c2);
+  if(r > 180){
+    r = a(r,360);
+  }
+  return r;
+}
 
 int main(){
   cout << adiff(180,270);
